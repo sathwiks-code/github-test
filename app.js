91bb1219409e8b94179853985fe3666c9b462a47
sathwiks-code <bsaisuryasathwik@gmail.com>
2021-05-22 13:51:22 +0530

    Really Final Commit

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const btnT = document.getElementById('btnT');
 const btnU = document.getElementById('btnU');
-const txt = document.getElementById('example');
+const txt1 = document.getElementById('example1');
+const txt2 = document.getElementById('example2');
+const txt3 = document.getElementById('example3');
 
 btnT.addEventListener('click', () => {
-    txt.style.display = 'none';
+    txt1.style.display = 'none';
+    txt2.style.display = 'none';
+    txt3.style.display = 'none';
 })
 btnU.addEventListener('click', () => {
-    txt.style.display = 'block';
+    txt1.style.display = 'block';
+    txt2.style.display = 'block';
+    txt3.style.display = 'block';
 })
